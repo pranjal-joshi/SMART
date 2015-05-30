bf1b85a65428e2b2828f9f17aed5f4fbc39c542d
Pranjal Joshi <joshi.pranjal5@gmail.com>
2015-05-30 14:49:10 +0530

    basic security encryption for login

--- SMART_CPU/smart.py
+++ SMART_CPU/smart.py
@@ -1,9 +1,12 @@
+#/usr/bin/python
+
 from SimpleHTTPServer import *
 import SocketServer
 from time import sleep
 import wiringpi
 import os
 import MySQLdb as mdb
+import base64 as security
 
 PORT = 80
 
@@ -14,23 +17,24 @@ dataPin = 5
 enable = 6
 
 io = wiringpi.GPIO(wiringpi.GPIO.WPI_MODE_PINS)
+con = mdb.connect("localhost","root","linux")
+cur = con.cursor()
 
 # HTTP handler for GET request
 class handler(SimpleHTTPRequestHandler):
 	def do_GET(self):
-		print "printing self.path = ", self.path
 		url = self.path
 		if(url.find("Pass") > 0):
                 	offset = url.find("Pass")
 	                offset = offset + 5
 	                substr = url[offset:offset+5]
-	                if(substr == "12345"):
+	                if(substr == str(loginKey())):
 	                        self.send_response(200)
 	                        self.end_headers()
 	                        self.wfile.write("grant")
 				print "LOGIN SUCCESSFUL."
 	                else:
-        	                self.send_response(401)
+        	                self.send_response(200)
 	                        self.end_headers()
 	                        self.wfile.write("denied")
 				print "LOGIN UNSUCCESSFUL."
@@ -56,9 +60,6 @@ class handler(SimpleHTTPRequestHandler):
 
 	        else:
 	                print "Invalid URL."
-			self.send_response(403)
-			self.end_headers()
-			self.wfile.write("Invalid URL. Forbidden!")
 
 		return
 
@@ -70,10 +71,32 @@ def initGPIO(dataPin, clkPin, latchPin, enable):
 	io.pinMode(latchPin,1)
 	io.pinMode(enable,1)
 	
+def initDB():
+	print "Initializing MySQL database."
+	cur.execute("show databases")
+	a = cur.fetchall()
+	a = str(a)
+	if(a.find("smart") > 0):
+		print "database found."
+	else:
+		print "database not found. creating..."
+		cur.execute("create database smart")
+
+def loginKey():
+	f = open("smart.key","r")
+	key = f.read()
+	f.close()
+	key = str(key)
+	index  = key.find('\n')
+	key = key[:index]
+	key = security.b16decode(key)
+	return key
 
 httpd = SocketServer.TCPServer(("",PORT),handler)
 print "\n\r[ Self Monitoring Analysing & Reporting Technology ] (S.M.A.R.T)"
 print "Initializing with SU access..."
 initGPIO(dataPin, clkPin, latchPin, enable)
+initDB()
 print "Attempting to start localhost HTTP server on port ", PORT,"..."
+print "Started listening to S.M.A.R.T app  with security key: " + loginKey()
 httpd.serve_forever()
