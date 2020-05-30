f8cb006b07f3d30931847a4d0c4951e80f569eb6
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-05-30 17:00:23 +0530

    constants header added

---
+++ v3/ESP/smart_v3/smartConstants.h
@@ -0,0 +1,17 @@
+/*
+ * SMART Node - v3.0
+ * AUTHOR   :   PRANJAL JOSHI
+ * DATE     :   30/05/2020
+ * INFO     :   Header for accessing SPIFFS for save/load JSON configs
+ * ALL RIGHTS RESERVED.
+ */
+
+#ifndef SMARTCONSTANTS_H
+#define SMARTCONSTANTS_H
+
+// WiFi Manager constants
+#define AP_SSID "SMART_"+String(ESP.getChipId())
+#define AP_PASS "12345678"
+#define MIN_SIG 10
+
+#endif
