9adc07818fa0cfc6077dc78d590e4132297ee565
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-05-30 17:24:27 +0530

    code in progress

--- v3/ESP/smart_v3/smart_v3.ino
+++ v3/ESP/smart_v3/smart_v3.ino
@@ -5,10 +5,50 @@
  * ALL RIGHTS RESERVED.
  */
 
-void setup() {
+#include <ESP8266WiFi.h>         
+#include <DNSServer.h>            
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include "smartFileSystem.h"
+#include "smartConstants.h"
 
+smartFileSystem fsys;
+smartFileSystemFlags_t flag;
+
+WiFiManagerParameter userName("username","Registered username..","",31);
+WiFiManagerParameter nodeName("nodename","Device name or Room name","",32);
+WiFiManagerParameter mqttIp("ip","IoT Server IP","",33);
+WiFiManagerParameter mqttPort("port","IoT Server port","",34);
+
+DynamicJsonDocument dj(1024);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  fsys.setDebug(true);
+  initWifiManager();
 }
 
 void loop() {
+  delay(10);
+}
 
+void initWifiManager() {
+  WiFiManager mgr;
+  mgr.setMinimumSignalQuality(MIN_SIG);
+  mgr.setTimeout(CON_TIMEOUT);
+  mgr.addParameter(&userName);
+  mgr.addParameter(&nodeName);
+  mgr.addParameter(&mqttIp);
+  mgr.addParameter(&mqttPort);
+  mgr.autoConnect(AP_SSID, AP_PASS);
+  mgr.setSaveConfigCallback(wifiManagerCallback);
+}
+
+void wifiManagerCallback() {
+  Serial.println("TRIGGEREEED");
+  fsys.addConfig("username", userName.getValue());
+  fsys.addConfig("nodename", nodeName.getValue());
+  fsys.addConfig("mqtt_ip", mqttIp.getValue());
+  fsys.addConfig("mqtt_port", mqttPort.getValue());
 }
