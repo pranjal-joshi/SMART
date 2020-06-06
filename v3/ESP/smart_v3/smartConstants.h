5feb68c93bb01475577f3a65524531f7cbf02d79
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-06-06 17:18:38 +0530

    Update smartConstants.h

--- v3/ESP/smart_v3/smartConstants.h
+++ v3/ESP/smart_v3/smartConstants.h
@@ -9,21 +9,21 @@
 #ifndef SMARTCONSTANTS_H
 #define SMARTCONSTANTS_H
 
+// Master serial debug control
+#define mDebug            true
+
 #define RST_DLY           3000U
+#define BOOT_DLY          2000U
 
 #define MQTT_SERVER_IP    "35.225.15.182"
 #define MQTT_SERVER_PORT  "1883"
 #define MQTT_DELAY        5000U
 #define RETAIN            true
 
-//PubSub topics and subscriptions
+// PubSub topics and subscriptions
 #define TOPIC_TEST          "smart/dev"
 
-// WiFi Manager constants
-#define AP_PASS           "12345678"
-#define MIN_SIG           10
-#define CON_TIMEOUT       30 //10*60
-
+// SmartFileSystem JSON key names
 #define CONF_SSID         "confSSID"
 #define CONF_PASS         "confPass"
 #define CONF_USERNAME     "confUserName"
@@ -33,14 +33,15 @@
 #define CONF_WIFI_CH      "confWifiChannel"
 
 // Mesh constants
-#define MESH_SSID         "SMART-Mesh"
-#define MESH_PASS         "mesh@smart_v3"
-#define MESH_PORT         4321
-#define MESH_HIDDEN       1
-#define MESH_QUALITY_THRESH -75   //dBm
+#define MESH_SSID             "SMART-Mesh"
+#define MESH_PASS             "mesh@smart_v3"
+#define MESH_PORT             4321
+#define MESH_HIDDEN           1
+#define MESH_QUALITY_THRESH   -75   // dBm
+#define INTERVAL_ROOT_CHECK   30    // Seconds
 
 // smartWebServer constants
-#define SMART_PASS        AP_PASS
+#define SMART_PASS        "12345678"    // Change later
 
 // JSON constants
 #define JSON_SMARTID      "smartId"
@@ -52,6 +53,7 @@
 #define SWITCHING_NODE          // Enable for main smart node
 //#define SENSOR_NODE           // Enable for sensor nodes (this should be work in Mesh Only)
 //#define FORCE_MESH            // Enable to force the node in mesh mode
+//#define FORCE_ROOT            // Enable to force the node as ROOT
 #define NO_OF_DEVICES           4
 
 #endif
