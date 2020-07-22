3adbdae84c585a31db2ae5a1259d363fe2c9db7c
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-07-22 21:53:50 +0530

    Update SwitchboardRow.dart

--- v3/App/SMART/lib/models/SwitchboardRow.dart
+++ v3/App/SMART/lib/models/SwitchboardRow.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 
 class SwitchboardRow {
   String deviceName;
+  String deviceDescription;
   bool deviceState;
   IconData deviceIcon;
 
   SwitchboardRow({
     this.deviceName,
+    this.deviceDescription,
     this.deviceState,
     this.deviceIcon,
   });
