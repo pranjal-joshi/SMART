95153093fe5fd88642697116edeba2506f3ba883
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-07-24 20:14:45 +0530

    Update JsonModel.dart

--- v3/App/SMART/lib/models/JsonModel.dart
+++ v3/App/SMART/lib/models/JsonModel.dart
@@ -16,8 +16,8 @@ class JsonAppToNodeSwitchState {
 
   Map<String, dynamic> toJson() {
     return {
-      JSON_TO : to,
-      JSON_FROM : from,
+      JSON_TO: to,
+      JSON_FROM: from,
       JSON_TYPE: type,
       JSON_SMARTID: smartId,
       JSON_TYPE_DATA: dataList
@@ -27,7 +27,6 @@ class JsonAppToNodeSwitchState {
   String toJsonString() {
     return jsonEncode(toJson());
   }
-  
 }
 
 class JsonNodeToAppSwitchState {
@@ -38,10 +37,36 @@ class JsonNodeToAppSwitchState {
   List<int> dataList;
 
   JsonNodeToAppSwitchState.fromJsonString(String rawJson) {
-    rawJson = rawJson.replaceAll('\'', '\"');
-    Map<String, dynamic> json = jsonDecode(rawJson);
-    topic = json[JSON_TOPIC];
-    smartId = json[JSON_SMARTID];
-    dataList = json[JSON_TYPE_DATA].cast<int>();
+    try {
+      rawJson = rawJson.replaceAll('\'', '\"');
+      Map<String, dynamic> json = jsonDecode(rawJson);
+      topic = json[JSON_TOPIC];
+      smartId = json[JSON_SMARTID];
+      dataList = json[JSON_TYPE_DATA].cast<int>();
+    } catch (e) {}
+  }
+}
+
+class JsonNodeInfo {
+  final String from = JSON_TO_NODE;
+  final String type = JSON_TYPE_INFO;
+  String deviceType;
+  String nodeName;
+  int nod;
+  String version;
+  String to;
+  String smartId;
+
+  JsonNodeInfo.fromJsonString(String rawJson) {
+    try {
+      rawJson = rawJson.replaceAll('\'', '\"');
+      Map<String, dynamic> json = jsonDecode(rawJson);
+      deviceType = json[JSON_DEVICE_TYPE];
+      nodeName = json[JSON_NODENAME];
+      nod = json[JSON_NoD];
+      version = json[JSON_VERSION];
+      to = json[JSON_TO];
+      smartId = json[JSON_SMARTID];
+    } catch (e) {}
   }
 }
