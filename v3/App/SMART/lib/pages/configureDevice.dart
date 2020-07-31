f4af6d23c3ec6bbb3c980ee3a7fb79cbbf9e083d
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-07-31 21:41:25 +0530

    WIP - Debug HttpException

--- v3/App/SMART/lib/pages/configureDevice.dart
+++ v3/App/SMART/lib/pages/configureDevice.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:webview_flutter/webview_flutter.dart';
 
 import '../models/SmartConstants.dart';
 
@@ -9,11 +13,35 @@ class ConfigureDevice extends StatefulWidget {
 
 class _ConfigureDeviceState extends State<ConfigureDevice> {
   @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<String> fetchWebpage() async {
+    var response = await http.get('http://192.168.4.1');
+    return response.body;
+  }
+
+  @override
   Widget build(BuildContext context) {
     Map<String, dynamic> args =
         ModalRoute.of(context).settings.arguments as Map<String, dynamic>;
     print(args['ssid']);
     print(args['info']);
+    //fetchWebpage().then((value) => print(value));
+
+    HttpClient client = HttpClient();
+    client.getUrl(Uri.parse('http://192.168.4.1/')).then(
+    // client.getUrl(Uri.parse('http://216.58.203.163/')).then(
+      (req) {
+        return req.close();
+      },
+    ).then(
+      (response) {
+        response.transform(utf8.decoder).listen(print);
+      },
+    );
+
     return Scaffold(
       body: SafeArea(
         child: Column(
