edba9b3a66e6a255958c3cb79690238a6c747bc7
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-08-10 15:23:36 +0530

    Dropdown widget added

---
+++ v3/App/SMART/lib/widgets/SmartDropdown.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+import '../models/SmartConstants.dart';
+
+class SmartDropdown extends StatefulWidget {
+  final IconData prefixIcon;
+  final double borderRadius;
+  final List<dynamic> itemList;
+  final String hint;
+  final Function onChanged;
+  dynamic selectedItem;
+
+  SmartDropdown({
+    this.borderRadius = 16,
+    this.hint = 'Please Select',
+    this.prefixIcon,
+    @required this.itemList,
+    @required this.selectedItem,
+    @required this.onChanged,
+  });
+
+  @override
+  _SmartDropdownState createState() => _SmartDropdownState();
+}
+
+class _SmartDropdownState extends State<SmartDropdown> {
+  SmartHelper helper;
+
+  @override
+  Widget build(BuildContext context) {
+    helper = SmartHelper(context: context);
+
+    return Row(
+      children: <Widget>[
+        Icon(
+          widget.prefixIcon,
+          color: Theme.of(context).primaryColorDark,
+          size: 32,
+        ),
+        Container(
+          width: helper.screenWidth - 32 - 48,
+          margin: const EdgeInsets.only(left: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 2),
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(widget.borderRadius),
+            border: Border.all(
+              width: 2,
+              color: Theme.of(context).primaryColorDark,
+            ),
+          ),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton(
+              icon: Icon(Icons.arrow_drop_down),
+              items: widget.itemList,
+              elevation: 2,
+              iconEnabledColor: Theme.of(context).primaryColorDark,
+              isExpanded: true,
+              hint: Text(
+                widget.hint,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryColorDark,
+                ),
+              ),
+              value: widget.selectedItem,
+              onChanged: (dynamic val) {
+                widget.onChanged(val);
+                setState(() => widget.selectedItem = val);
+              },
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
