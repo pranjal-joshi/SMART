709769686bea107b9484e35adfda79184a87c2ac
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-08-14 20:19:30 +0530

    minor improvements

--- v3/App/SMART/lib/models/SmartConstants.dart
+++ v3/App/SMART/lib/models/SmartConstants.dart
@@ -320,3 +320,4 @@ const String SP_SmartMqttPublishBuffer = "sp_SmartMqttPublishBuffer";
 const String SP_SmartRoomData = "sp_SmartRoomData";
 const String SP_LoginState = "sp_LoginState";
 const String SP_LoginCredentials = "sp_LoginCredentials";
+const String SP_DisplayName = "sp_DisplayName";
