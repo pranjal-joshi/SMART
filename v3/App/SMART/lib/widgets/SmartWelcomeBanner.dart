23c929c1b68c774261b456923baaaf7e973b3cfd
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-08-16 15:54:51 +0530

    WelcomeBanner widget created

---
+++ v3/App/SMART/lib/widgets/SmartWelcomeBanner.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+import '../controllers/SmartSharedPref.dart';
+
+class SmartWelcomeBanner extends StatefulWidget {
+  @override
+  _SmartWelcomeBannerState createState() => _SmartWelcomeBannerState();
+}
+
+class _SmartWelcomeBannerState extends State<SmartWelcomeBanner> {
+  final sp = SmartSharedPreference();
+  final randomizer = Random();
+  String _displayMessage;
+
+  final messages = [
+    'Welcome, ',
+    'Hello, ',
+    'Good to See You,',
+    'Glad You Came Back, ',
+    'Greetings!',
+    'Hi there,',
+  ];
+
+  @override
+  void initState() {
+    _displayMessage = _welcomeMessage;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _loadCredentials(),
+      initialData: ['', '', 'User'],
+      builder: (_, snapshot) {
+        return RichText(
+          text: TextSpan(
+            text: _displayMessage,
+            style: TextStyle(
+              fontSize: 28,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+              fontFamily: 'ProductSans',
+            ),
+            children: [
+              TextSpan(
+                text: '\n${snapshot.data[2]}',
+                style: TextStyle(
+                  fontSize: 42,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Future<List<String>> _loadCredentials() async {
+    List<String> list = await sp.loadLoginCredentials();
+    return list;
+  }
+
+  String get _welcomeMessage {
+    return messages[randomizer.nextInt(messages.length)];
+  }
+}
