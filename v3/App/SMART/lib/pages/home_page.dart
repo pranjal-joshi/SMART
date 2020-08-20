df37b55e6efc2e80d495d49f1c451402e7f29cde
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-08-20 21:31:33 +0530

    Stack UI improved! Setup widget added

--- v3/App/SMART/lib/pages/home_page.dart
+++ v3/App/SMART/lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 
 import '../controllers/SmartMqtt.dart';
@@ -62,137 +63,6 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     helper = SmartHelper(context: context);
 
-    /*return Scaffold(
-      body: SafeArea(
-        top: false,
-        child: Container(
-          width: double.infinity,
-          height: helper.screenHeight,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                flex: 6,
-                child: LayoutBuilder(
-                  builder: (_, constraints) {
-                    return Stack(
-                      clipBehavior: Clip.antiAliasWithSaveLayer,
-                      children: [
-                        Align(
-                          alignment: Alignment.topCenter,
-                          child: Container(
-                            width: double.infinity,
-                            height: constraints.maxHeight / 1.2,
-                            decoration: helper.isDarkModeActive
-                                ? BoxDecoration(
-                                    color: Colors.black,
-                                  )
-                                : BoxDecoration(
-                                    gradient: LinearGradient(
-                                      tileMode: TileMode.mirror,
-                                      begin: Alignment.topCenter,
-                                      end: Alignment.bottomCenter,
-                                      colors: [
-                                        Theme.of(context).primaryColorDark,
-                                        Colors.indigo[700],
-                                      ],
-                                    ),
-                                    borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(10),
-                                      bottomRight: Radius.circular(10),
-                                    ),
-                                  ),
-                            child: Padding(
-                              padding: EdgeInsets.only(
-                                left: 12,
-                                right: 12,
-                                top: 12 + MediaQuery.of(context).padding.top,
-                              ),
-                              child: Row(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Expanded(
-                                    child: Padding(
-                                      padding: const EdgeInsets.only(right: 12),
-                                      child: SmartWelcomeBanner(),
-                                    ),
-                                  ),
-                                  CircleAvatar(
-                                    backgroundColor:
-                                        Theme.of(context).canvasColor,
-                                    radius: 28,
-                                    child: Text('DP'),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                        Consumer<JsonNodeStatusProvider>(
-                          builder: (_, status, __) {
-                            return SmartWelcomeCard(
-                              helper: helper,
-                              constraints: constraints,
-                              title: status.getActiveMessage['title'],
-                              subtitle: status.getActiveMessage['subtitle'],
-                              onActionTap: () => Navigator.of(context)
-                                  .pushNamed(route_addNewDevice),
-                            );
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                ),
-              ),
-              Expanded(
-                flex: 15,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 8),
-                  child: FutureBuilder(
-                    initialData: roomDataList,
-                    future: loadRooms(),
-                    builder: (_, AsyncSnapshot<List<SmartRoomData>> snapshot) {
-                      return GridView.builder(
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 0,
-                          mainAxisSpacing: 24,
-                          childAspectRatio: 5 / 4,
-                        ),
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (_, index) {
-                          return Consumer<JsonRoomStateProvider>(
-                            builder: (_, stateData, __) => Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 14),
-                              child: SmartRoomCard(
-                                helper: helper,
-                                roomData: snapshot.data[index],
-                                indicatorState: stateData.roomPowerIndicatorMap[
-                                    snapshot.data[index].name],
-                                onTap: () {
-                                  print(
-                                      'Clicked on ${snapshot.data[index].name}');
-                                  Navigator.of(context)
-                                      .pushNamed(route_room_page);
-                                },
-                              ),
-                            ),
-                          );
-                        },
-                      );
-                    },
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );*/
-
     return Scaffold(
       body: SafeArea(
         top: false,
@@ -205,15 +75,17 @@ class _HomePageState extends State<HomePage> {
                 width: helper.screenWidth,
                 height: helper.screenHeight,
                 child: Padding(
-                  padding: EdgeInsets.only(top: helper.screenHeight/3),
+                  padding: EdgeInsets.only(top: helper.screenHeight / 3.3),
                   child: FutureBuilder(
                     initialData: roomDataList,
                     future: loadRooms(),
                     builder: (_, AsyncSnapshot<List<SmartRoomData>> snapshot) {
+                      if (snapshot.data.length == 0) {
+                        return _getNoRoomLayout();
+                      }
                       return GridView.builder(
                         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                           crossAxisCount: 2,
-                          crossAxisSpacing: 0,
                           mainAxisSpacing: 24,
                           childAspectRatio: 5 / 4,
                         ),
@@ -324,6 +196,34 @@ class _HomePageState extends State<HomePage> {
               ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget _getNoRoomLayout() {
+    return InkWell(
+      onTap: () => Navigator.of(context).pushNamed(route_addNewDevice),
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SvgPicture.asset(
+              'assets/images/next_option.svg',
+              fit: BoxFit.contain,
+              width: helper.screenWidth * 0.8,
+            ),
+            const Text(
+              "Tap Here to Setup your First S.M.A.R.T Device",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32,
+                color: Colors.grey,
+                letterSpacing: 0.5,
+              ),
+            ),
+          ],
         ),
       ),
     );
