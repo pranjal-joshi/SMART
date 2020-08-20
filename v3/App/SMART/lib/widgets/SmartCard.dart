02acec47a373a7c9a8c5d62c5e4797bdd494e4b7
Pranjal Joshi <joshi.pranjal5@gmail.com>
2020-08-20 17:58:20 +0530

    Custom Card Widget with Subtle Shadow

---
+++ v3/App/SMART/lib/widgets/SmartCard.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+import '../helpers/SmartHelper.dart';
+
+enum SmartCardBlurRadius {
+  Flat,
+  Subtle,
+  Spread14,
+  Spread22,
+}
+
+class SmartCard extends StatelessWidget {
+  final Color shadowColor;
+  final double cornerRadius;
+  final Widget child;
+  final EdgeInsets padding;
+  final SmartHelper helper;
+  final double elevation;
+  final SmartCardBlurRadius blurRadius;
+
+  SmartCard({
+    this.shadowColor,
+    this.cornerRadius = 4,
+    this.elevation = 8,
+    this.padding = const EdgeInsets.all(0),
+    @required this.child,
+    @required this.helper,
+    this.blurRadius = SmartCardBlurRadius.Subtle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Color _colorToUse;
+    double _blurRadiusToUse;
+    switch (blurRadius) {
+      case SmartCardBlurRadius.Flat:
+        _blurRadiusToUse = 0;
+        break;
+      case SmartCardBlurRadius.Subtle:
+        _blurRadiusToUse = 8;
+        break;
+      case SmartCardBlurRadius.Spread14:
+        _blurRadiusToUse = 14;
+        break;
+      case SmartCardBlurRadius.Spread22:
+        _blurRadiusToUse = 22;
+        break;
+    }
+    shadowColor == null
+        ? _colorToUse = Colors.grey[400]
+        : _colorToUse = shadowColor.withOpacity(0.35);
+    if (helper.isDarkModeActive) {
+      return Container(
+        child: ClipRRect(
+          child: child,
+          clipBehavior: Clip.antiAlias,
+          borderRadius: BorderRadius.all(Radius.circular(cornerRadius)),
+        ),
+        padding: padding,
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.all(Radius.circular(cornerRadius)),
+        ),
+      );
+    }
+    return Container(
+      child: ClipRRect(
+        child: child,
+        clipBehavior: Clip.antiAlias,
+        borderRadius: BorderRadius.all(Radius.circular(cornerRadius)),
+      ),
+      padding: padding,
+      decoration: BoxDecoration(
+        color: Theme.of(context).cardColor,
+        borderRadius: BorderRadius.all(Radius.circular(cornerRadius)),
+        boxShadow: blurRadius != SmartCardBlurRadius.Flat
+            ? [
+                BoxShadow(
+                  blurRadius: _blurRadiusToUse,
+                  spreadRadius: -4,
+                  color: _colorToUse,
+                  offset: Offset(0, elevation),
+                ),
+                BoxShadow(
+                  blurRadius: _blurRadiusToUse,
+                  spreadRadius: -4,
+                  color: _colorToUse,
+                  offset: Offset(0, -elevation / 80),
+                ),
+                BoxShadow(
+                  blurRadius: _blurRadiusToUse,
+                  spreadRadius: -4,
+                  color: _colorToUse,
+                  offset: Offset(elevation / 6, 0),
+                ),
+              ]
+            : null,
+      ),
+    );
+  }
+}
