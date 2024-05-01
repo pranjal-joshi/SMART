ff99af6536c96a417d811f0a01c01ea0e466a844
Pranjal Joshi <joshi.pranjal5@gmail.com>
2024-05-01 14:06:57 +0530

    Create creds.h

---
+++ v4_alexa_based/creds.h
@@ -0,0 +1,8 @@
+const char* ssid1 = "";
+const char* pass1 = "";
+const char* ssid2 = "";
+const char* pass2 = "#";
+
+const char* device_id = "ESP-MotionSensor-Workdesk";
+const String invoke_endpoint_motion = "http://www.virtualsmarthome.xyz/url_routine_trigger/<KEY>";
+const String invoke_endpoint_no_motion = "http://www.virtualsmarthome.xyz/url_routine_trigger/<KEY>";
