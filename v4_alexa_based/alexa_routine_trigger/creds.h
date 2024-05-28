462a4db0a2053a4f8fd0e2ce52010fa672d432eb
Pranjal Joshi <joshi.pranjal5@gmail.com>
2024-05-28 20:17:24 +0530

    Separate config for ESP01-S

--- v4_alexa_based/alexa_routine_trigger/creds.h
+++ v4_alexa_based/alexa_routine_trigger/creds.h
@@ -2,6 +2,7 @@
 
 #define DEBUG 0
 #define NODEMCU 0
+#define ESP01_S 1
 
 #if NODEMCU == 1
   #undef ESP01
@@ -10,11 +11,19 @@
   #undef NODEMCU
 #endif
 
+#if ESP01_S == 0
+  #undef ESP01_S
+#endif
+
 #if DEBUG == 0
   #undef DEBUG
 #endif
 
-String VERSION = "1.0.0";
+#if ESP01_S == 1
+  String VERSION = "1.0.0s";
+#else
+  String VERSION = "1.0.0";
+#endif
 
 String calculate_ssid = "SmartMotion-" + String(ESP.getChipId());
 const char* ssid_provision = calculate_ssid.c_str();
